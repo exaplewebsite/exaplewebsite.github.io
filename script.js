053316460ea6cb28626ba5c78e7327799b08ea8f
exaplewebsite <130406650+exaplewebsite@users.noreply.github.com>
2023-04-16 12:08:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const intensityButtons = document.querySelectorAll(".intensity__list__item__btn");
+const informationButton = document.querySelector(".rank__information");
+const informationWindow = document.querySelector(".information");
+const informationClose = document.querySelector(".information__window__btn");
 let isChoosen = false;
 let choosenElement;
+const fadeIn = (el, timeout, display) =>{
+    el.style.opacity = 0;
+    el.style.display = display || 'block';
+    el.style.transition = `opacity ${timeout}ms`
+    setTimeout(() => {
+        el.style.opacity = 1;
+    }, 10);
+};
+
+const fadeOut = (el, timeout) =>{
+    el.style.opacity = 1;
+    el.style.transition = `opacity ${timeout}ms`
+    el.style.opacity = 0;
+
+    setTimeout(() => {
+        el.style.display = 'none';
+    },timeout);
+};
+
 intensityButtons.forEach(e => {
     e.addEventListener("click", function() {
         if (isChoosen){
@@ -13,4 +35,12 @@ intensityButtons.forEach(e => {
             choosenElement = e;
         }
     })
+})
+
+informationButton.addEventListener("click", function() {
+    fadeIn(informationWindow, 1000);
+})
+
+informationClose.addEventListener("click", function() {
+    fadeOut(informationWindow, 1000);
 })
--- slick.js
+++ slick.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
 $('.achievements__list').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1,
     arrows: false,
+    slidesToScroll: 6,
     swipeToSlide: true,
-    adaptiveHeight: true
 });
 });
