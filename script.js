630ac0c3506269fd27453a3ffb473cc8c8cce354
exaplewebsite <130406650+exaplewebsite@users.noreply.github.com>
2023-05-18 23:38:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ const sixthAchievementsContent = '<div id="six" class="achievements__list__item_
 const achievementsContent = [firstAchievementsContent, secondAchievementsContent, thirdAchievementsContent, fourthAchievementsContent, fifthAchievementsContent, sixthAchievementsContent];
 let isChoosen = false;
 let choosenElement;
-let clientID = window.Telegram.WebApp.initDataUnsafe.user.id;
+// let clientID = window.Telegram.WebApp.initDataUnsafe.user.id;
+let clientID = 1086925635;
 let night = false;
 let choosenAch = informationText;
 let dayP;
